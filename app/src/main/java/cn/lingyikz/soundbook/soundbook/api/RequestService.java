d5a6eb86e5b0b7e443f8d06fa673d630af6096c8
1085054孟凡威 <1085054@yjpzfoods.com>
2022-09-02 14:03:03 +0800

    修改为正式版地址

--- app/src/main/java/cn/lingyikz/soundbook/soundbook/api/RequestService.java
+++ app/src/main/java/cn/lingyikz/soundbook/soundbook/api/RequestService.java
@@ -7,7 +7,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class RequestService {
     private static Retrofit retrofit ;
     private volatile static RequestService requestService ;
-    public static final String BASE_URL = "http://lingyikz.qicp.vip/" ;
+//    public static final String BASE_URL = "http://lingyikz.qicp.vip/" ;
+    public static final String BASE_URL = "https://app-api.lingyikz.cn/" ;
+
 
     private RequestService(){
 
